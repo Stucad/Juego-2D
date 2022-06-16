cc1e2ebb8e48bedccda30bacbd15fe8b4bcef01c
Stuart <Stuartcardenasduarte@gmail.com>
2022-06-16 15:06:29 -0500

    Primer Commit
    Inicio del proyecto

---
+++ ZombiLand/Assets/Scripts/Move.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] private float velocidadMovimiento;
+    [SerializeField] private Transform[] puntosMovimiento;
+    [SerializeField] private float distanciaMinima;
+
+    private int numeroAleatorio;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+
+        numeroAleatorio = Random.Range(0, puntosMovimiento.Length);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        Girar();
+
+    }
+
+    private void Update()
+    {
+
+        transform.position = Vector2.MoveTowards(transform.position, puntosMovimiento[numeroAleatorio].position, velocidadMovimiento * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, puntosMovimiento[numeroAleatorio].position) < distanciaMinima)
+        {
+
+            numeroAleatorio = Random.Range(0, puntosMovimiento.Length);
+
+            Girar();
+        }
+
+    } 
+
+    private void Girar()
+    {
+        if(transform.position.x < puntosMovimiento[numeroAleatorio].position.x)
+        {
+
+            spriteRenderer.flipX = true;
+
+        }
+        else
+        {
+
+            spriteRenderer.flipX = false;
+
+        }
+    }
+
+}
