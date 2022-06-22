13e8f28893f5d7f52e1fb19be6551d2bae19c1c0
Stuart <Stuartcardenasduarte@gmail.com>
2022-06-22 14:52:17 -0500

    Cuarto Commit
    Vida del jugador

---
+++ ZombiLand/Assets/Scripts/Hearts.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Hearts : MonoBehaviour
+{
+    public GameObject[] heart;
+
+    public int life;
+
+    public GameObject[] enemys;
+
+    
+    public void Update()
+    {
+        
+        if(life < 1)
+        {
+
+            Destroy(heart[0].gameObject);
+
+        }
+        else if(life < 2)
+        {
+
+            Destroy(heart[1].gameObject);
+
+        }
+        else if(life < 3)
+        {
+
+            Destroy(heart[2].gameObject);
+
+        }
+
+
+    }
+    
+    private void OnBecameInvisible()
+    {
+        if(tag == "Human")
+        {
+            life--;
+            Destroy(heart[1]);
+
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if(collision.tag == "enemy")
+        {
+
+            life--;
+            Destroy(collision.gameObject);
+        }
+
+
+    }
+}
